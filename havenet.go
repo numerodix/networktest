663fdbb14be06e8f86c90cd3543a13dcbcd63ac2
Martin Matusiak <numerodix@gmail.com>
2016-07-10 16:08:39 +0200

    working goroutine

--- havenet.go
+++ havenet.go
@@ -1,13 +1,13 @@
 package main
 
 import (
-//    "bytes"
-    "errors"
-//    "fmt"
+    "bytes"
+//    "errors"
+    "fmt"
     "log"
-//    "os/exec"
-//    "regexp"
-//    "strconv"
+    "os/exec"
+    "regexp"
+    "strconv"
 //    "strings"
 )
 
@@ -19,21 +19,52 @@ type CommandResult struct {
 }
 
 
-func Ping(pingch chan CommandResult) {
-    fvalue := 0.0
-    err := errors.New("")
+func Ping(pingch chan CommandResult, host string, cnt int, timeout int) {
+    // Prepare return variables
+    etime := -1.0
 
-    pingch <- CommandResult{FValue: fvalue, Error: err}
+    // Construct the args
+    var executable = "ping"
+    var args []string
+    args = append(args, fmt.Sprintf("-c%d", cnt))
+    args = append(args, fmt.Sprintf("-W%d", timeout))
+    args = append(args, host)
+
+    // Construct the cmd
+    cmd := exec.Command(executable, args...)
+    var out bytes.Buffer
+    cmd.Stdout = &out
+
+    // Invoke the cmd
+    err := cmd.Run()
+    if err != nil {
+        pingch <- CommandResult{FValue: etime, Error: err}
+        return
+    }
+
+    // Parse the time= value
+    var stdout = out.String()
+    rx := regexp.MustCompile("time=([^ ]*)")
+    var time_s = rx.FindStringSubmatch(stdout)[1]
+    var time, err2 = strconv.ParseFloat(time_s, 64)
+    if err2 != nil {
+        pingch <- CommandResult{FValue: etime, Error: err2}
+        return
+    }
+
+    pingch <- CommandResult{FValue: time, Error: nil}
 }
 
 
 func main() {
 //    hosts := []string{"yahoo.com", "google.com"}
-//    hosts := []string{"localhost"}
+    hosts := []string{"localhost"}
 
-    pingch := make(chan CommandResult)
-    go Ping(pingch)
-    pingres := <-pingch
+    for i := range hosts {
+        pingch := make(chan CommandResult)
+        go Ping(pingch, hosts[i], 1, 2)
+        pingres := <-pingch
 
-    log.Println("time: ", pingres.FValue)
+        log.Println("time: ", pingres.FValue)
+    }
 }
