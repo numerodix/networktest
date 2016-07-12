9175d7e50cb29d2305fd1293f83b2e045ce05d36
Martin Matusiak <numerodix@gmail.com>
2016-07-12 21:47:04 +0200

    remove prototyping code

--- src/havenet.go
+++ src/havenet.go
@@ -12,92 +12,47 @@ import (
 )
 
 
-type CommandResult struct {
-    Id string
-    FValue float64
-    SValue string
-    Error error
+func PingJob(ch chan PingExecution, host string, cnt int, timeout int) {
+    ch <- Ping(host, cnt, timeout)
 }
 
 
-func PingProc(ch chan CommandResult, host string, cnt int, timeout int) {
-    pingExec := Ping(host, cnt, timeout)
-    if pingExec.Error != nil {
-        ch <- CommandResult{Id: pingExec.Host, Error: pingExec.Error}
-        return
+func SpawnAndCollect(pingHosts map[string]PingExecution) {
+    ch := make(chan PingExecution)
+
+    // Launch
+    for ip := range pingHosts {
+        go PingJob(ch, ip, 1, 2)
     }
 
-    ch <- CommandResult{Id: pingExec.Host, FValue: pingExec.Time}
+    // Collect
+    for ip := range pingHosts {
+        pingExec := <-ch
+
+        pingHosts[pingExec.Host] = pingExec
+
+        // To make it not complain about ip not being used lol
+        if false {
+            fmt.Printf("%s", ip)
+        }
+    }
 }
 
 
 func main() {
-//    hosts := []string{"yahoo.com", "google.com"}
-    hosts := []string{
-        "192.168.1.1",
-/*        "192.228.79.201",
-        "127.0.0.1",
-        "127.0.1.1",
-        "localhost",
-        "yahoo.com",
-        "google.com",
-        "juventuz.com",
-        "twitter.com",
-        "facebook.com",
-        "gmail.com",
-        "golang.org",
-        "www.nu.nl",
-        "www.aftenposten.no",
-        "www.bonjourchine.com",
-        "github.com",
-        "youtube.com",
-*/
-    }
-//    hosts := []string{"localhost"}
-    ch := make(chan CommandResult)
-
-    // Launch
-    for i := range hosts {
-        go PingProc(ch, hosts[i], 1, 2)
-    }
-
-    // Collect
-    sum := 0.0
-    for i := range hosts {
-        cmdres := <-ch
-
-        if cmdres.Error != nil {
-            fmt.Printf("Err: %s: %s\n", cmdres.Id, cmdres.Error)
-            continue
-        }
-
-        sum += cmdres.FValue
-        fmt.Printf("%-2d  %-34s: %.1f ms\n", i, cmdres.Id, cmdres.FValue)
-    }
-
-    fmt.Printf("Total time: %.1f ms\n", sum)
-
-    fmt.Printf("ping exec: %s\n", Ping("localhost", 1, 1))
-
-
     var route = Route()
-    fmt.Printf("networks: %s\n", route.GetNetworks())
-    fmt.Printf("gateways: %s\n", route.GetGateways())
-
     var ifconfig = Ifconfig()
-    fmt.Printf("ifconfig: %s\n", ifconfig)
-
 
 
     // Do local pings
-    var localPings = make(map[string]PingExecution)
+    var netPings = make(map[string]PingExecution)
 
     var ifaceBlocks = ifconfig.IfaceBlocks
     for i := range ifaceBlocks {
         var ifaceBlock = ifaceBlocks[i]
         var ip = ifaceBlock.IPv4
 
-        localPings[ip] = Ping(ip, 1, 2)
+        netPings[ip] = PingExecution{}
     }
 
     var gws = route.GetGateways()
@@ -105,14 +60,57 @@ func main() {
         var gw = gws[i]
         var ip = gw.Gateway
 
-        localPings[ip] = Ping(ip, 1, 2)
+        netPings[ip] = PingExecution{}
     }
 
+    SpawnAndCollect(netPings)
+
+
+    // Do remote pings
+    inetDnsServers := make(map[string]string)
+    inetDnsServers["b.root-servers.net."] = "192.228.79.201"
+
+    netDnsServers := []string{
+        "127.0.1.1",
+    }
+
+    inetHosts := []string{
+        "facebook.com",
+        "github.com",
+        "gmail.com",
+        "google.com",
+        "twitter.com",
+        "nu.nl",
+        "yahoo.com",
+        "youtube.com",
+    }
+
+    var inetPings = make(map[string]PingExecution)
+
+    for _, ip := range inetDnsServers {
+        inetPings[ip] = PingExecution{}
+    }
+
+    for i := range netDnsServers {
+        var host = netDnsServers[i]
+        inetPings[host] = PingExecution{}
+    }
+
+    for i := range inetHosts {
+        var host = inetHosts[i]
+        inetPings[host] = PingExecution{}
+    }
+
+    SpawnAndCollect(inetPings)
 
 
 
 
     col := ColorBrush{enabled:true}
+
+    /* 
+        LOCAL NETWORK
+    */
 
     fmt.Printf(col.yellow(" + Scanning for networks...\n"))
     var networks = route.GetNetworks()
@@ -129,14 +127,13 @@ func main() {
     }
 
     fmt.Printf(col.yellow(" + Detecting ips...\n"))
-//    var ifaceBlocks = ifconfig.IfaceBlocks
     for i := range ifaceBlocks {
         var ifaceBlock = ifaceBlocks[i]
 
         var iface = fmt.Sprintf("<%s>", ifaceBlock.Iface)
         var ip = ifaceBlock.IPv4
         var mask = ifaceBlock.Mask
-        var ping = localPings[ip].Time
+        var ping = netPings[ip].Time
         var ifaceS = col.magenta(fmt.Sprintf("%-10s", iface))
         var ipS = col.green(fmt.Sprintf("%-15s", ip))
         var maskS = col.cyan(fmt.Sprintf("/ %-15s", mask))
@@ -145,17 +142,50 @@ func main() {
     }
 
     fmt.Printf(col.yellow(" + Detecting gateways...\n"))
-//    var gws = route.GetGateways()
     for i := range gws {
         var gw = gws[i]
 
         var iface = fmt.Sprintf("<%s>", gw.Iface)
         var ip = gw.Gateway
-        var ping = localPings[ip].Time
+        var ping = netPings[ip].Time
         var ifaceS = col.magenta(fmt.Sprintf("%-10s", iface))
         var ipS = col.green(fmt.Sprintf("%-15s", ip))
         var pingS = col.green(fmt.Sprintf("%.3f ms", ping))
         fmt.Printf("    %s  %s   ping: %s\n", ifaceS, ipS, pingS)
     }
 
+    /* 
+        INTERNET
+    */
+
+    fmt.Printf(col.yellow(" + Testing internet connection...\n"))
+    for name, ip := range inetDnsServers {
+        var ping = inetPings[ip].Time
+        var nameS = col.cyan(fmt.Sprintf("%s", name))
+        var ipS = col.green(fmt.Sprintf("%-15s", ip))
+        var pingS = col.green(fmt.Sprintf("%.1f ms", ping))
+        fmt.Printf("    %s  %s   ping: %s\n", nameS, ipS, pingS)
+    }
+
+    fmt.Printf(col.yellow(" + Detecting dns servers...\n"))
+    for i := range netDnsServers {
+        var host = netDnsServers[i]
+
+        var ip = host
+        var ping = inetPings[host].Time
+        var ipS = col.green(fmt.Sprintf("%-15s", ip))
+        var pingS = col.green(fmt.Sprintf("%.1f ms", ping))
+        fmt.Printf("    %s   ping: %s\n", ipS, pingS)
+    }
+
+    fmt.Printf(col.yellow(" + Testing internet dns...\n"))
+    for i := range inetHosts {
+        var host = inetHosts[i]
+
+        var ip = host
+        var ping = inetPings[host].Time
+        var ipS = col.green(fmt.Sprintf("%-15s", ip))
+        var pingS = col.green(fmt.Sprintf("%.1f ms", ping))
+        fmt.Printf("    %s   ping: %s\n", ipS, pingS)
+    }
 }
