ac5fed21fe2970f00938bfc4e91016ae9e14f2d7
Martin Matusiak <numerodix@gmail.com>
2016-07-21 18:15:21 +0200

    add assert functionality

--- src/assert.go
+++ src/assert.go
@@ -1,22 +1,78 @@
 package main
 
+import "fmt"
+import "runtime"
+import "strings"
 import "testing"
+
+
+// Peek up the call stack to find the name of the calling function
+func _getFuncPtrVal() uintptr {
+    // frame 0 : assertXXX
+    // frame 1 : TestXXX <- the one we want
+    // frame 2 : testing.tRunner
+    var frameCnt = 3
+
+    // Extract the top few frames
+    var pc = make([]uintptr, frameCnt)
+    runtime.Callers(frameCnt, pc)
+
+    return pc[1]
+}
+
+// Return the function name for the calling function
+func _getFuncName() string {
+    var ptrVal = _getFuncPtrVal()
+
+    // Look up the function object
+    var fun = runtime.FuncForPC(ptrVal)
+
+    // returns: _/home/user/src/gohavenet/src.TestProcMgr_False
+    var funcNamePath = fun.Name()
+
+    // Split on the slash and return just the func name
+    var pathElems = strings.Split(funcNamePath, "/")
+    var index = len(pathElems) - 1
+    if index < 0 {
+        index = 0
+    }
+
+    return pathElems[index]
+}
+
+// Return /path/to/file:12 for the calling function
+func _getFuncFileLine() string {
+    var ptrVal = _getFuncPtrVal()
+
+    // Look up the function object
+    var fun = runtime.FuncForPC(ptrVal)
+    var file, line = fun.FileLine(ptrVal)
+
+    line = line - 1  // line seems to be one too many
+    var fileLine = fmt.Sprintf("%s:%d", file, line)
+
+    return fileLine
+}
+
 
 
 func assertIntEq(t *testing.T, a, b int, msg string) {
     if a != b {
-        t.Errorf("Integers do not match: %d != %d [%s]", a, b, msg)
+        t.Errorf("%s: Integers do not match: %d != %d [%s] at %s",
+                 _getFuncName(), a, b, msg, _getFuncFileLine())
     }
 }
 
 func assertPtrIsNull(t *testing.T, a interface{}, msg string) {
     if a != nil {
-        t.Errorf("Pointer is not nil: %d, {%q} [%s]", a, a, msg)
+        t.Errorf("%s: Pointer is not nil: %d, {%q} [%s] at %s",
+                 _getFuncName(), a, a, msg, _getFuncFileLine())
     }
 }
 
 func assertStrEq(t *testing.T, a, b string, msg string) {
     if a != b {
-        t.Errorf("Strings do not match: '%s' != '%s' [%s]", a, b, msg)
+        t.Errorf("%s: Strings do not match: '%s' != '%s' [%s] at %s",
+                 _getFuncName(), a, b, msg, _getFuncFileLine())
     }
 }
