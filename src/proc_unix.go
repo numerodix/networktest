1505936509cd2defcd0597bcbd2eff3c4e467cba
Martin Matusiak <numerodix@gmail.com>
2016-07-23 11:38:23 +0200

    add runWithTimeout

--- src/proc_unix.go
+++ src/proc_unix.go
@@ -4,11 +4,13 @@ import "bytes"
 //import "fmt"
 import "os"
 import "os/exec"
+import "time"
 
 
 type ProcessManager struct {
     exe string
     args []string
+    timeoutMs int
 }
 
 
@@ -19,6 +21,7 @@ func ProcMgr(exe string, args... string) ProcessManager {
     }
     return mgr
 }
+
 
 func (mgr *ProcessManager) run() ProcessResult {
     // Construct the cmd
@@ -33,6 +36,58 @@ func (mgr *ProcessManager) run() ProcessResult {
     // Invoke the cmd
     os.Setenv("LC_ALL", "C")  // Make sure we don't get localized output
     var err = cmd.Run()
+
+    // Capture stdout, stderr
+    var stderr = errBuffer.String()
+    var stdout = outBuffer.String()
+
+    // Construct a result
+    var res = ProcessResult{
+        stderr: stderr,
+        stdout: stdout,
+        err: err,
+    }
+
+    return res
+}
+
+
+func (mgr *ProcessManager) runWithTimeout() ProcessResult {
+    // Construct the cmd
+    var cmd = exec.Command(mgr.exe, mgr.args...)
+
+    // Set up buffers for stdout, stderr
+    var errBuffer bytes.Buffer
+    var outBuffer bytes.Buffer
+    cmd.Stderr = &errBuffer
+    cmd.Stdout = &outBuffer
+
+    // Invoke the cmd
+    os.Setenv("LC_ALL", "C")  // Make sure we don't get localized output
+    var err = cmd.Start()
+
+    var elapsedMs = 0
+    for {
+        // We reached the timeout, so kill the process and return an error
+        if mgr.timeoutMs > 0 && elapsedMs >= mgr.timeoutMs {
+            err = cmd.Process.Kill()
+            break
+        }
+
+        // Use the pid to detect if the process exists
+        var _, err = os.FindProcess(cmd.Process.Pid)
+
+        // If it doesn't:
+        // - it's still forking (not yet running)
+        // - or it exited already (we check stdout to see if it's non-empty)
+        if err == nil && (outBuffer.String() != "" || errBuffer.String() != "") {
+            break
+        }
+
+        // The process hasn't finished yet, so wait a while and loop around
+        time.Sleep(50 * time.Millisecond)
+        elapsedMs += 50
+    }
 
     // Capture stdout, stderr
     var stderr = errBuffer.String()
--- src/proc_unix_test.go
+++ src/proc_unix_test.go
@@ -5,7 +5,7 @@ import "strings"
 import "testing"
 
 
-func Test_ProcMgr_false(t *testing.T) {
+func Test_ProcMgr_run_false(t *testing.T) {
     var mgr = ProcMgr("false")
     var res = mgr.run()
 
@@ -16,20 +16,20 @@ func Test_ProcMgr_false(t *testing.T) {
     assertStrEq(t, "exit status 1", res.err.Error(), "err does not match")
 }
 
-func Test_ProcMgr_ls(t *testing.T) {
+func Test_ProcMgr_run_ls(t *testing.T) {
     var mgr = ProcMgr("ls", "/dyhfi8345rh")
     var res = mgr.run()
 
     // See that we don't get localized output
     assertStrEq(t, "ls: cannot access /dyhfi8345rh: No such file or directory",
                     strings.TrimSpace(res.stderr), "stderr does not match")
-    assertStrEq(t, "", strings.TrimSpace(res.stdout), "stdout does not match")
+    assertStrEq(t, "", res.stdout, "stdout does not match")
 
     assertPtrNotNull(t, res.err, "err does not match")
     assertStrEq(t, "exit status 2", res.err.Error(), "err does not match")
 }
 
-func Test_ProcMgr_true(t *testing.T) {
+func Test_ProcMgr_run_true(t *testing.T) {
     var mgr = ProcMgr("true")
     var res = mgr.run()
 
@@ -39,12 +39,34 @@ func Test_ProcMgr_true(t *testing.T) {
     assertPtrIsNull(t, res.err, "err does not match")
 }
 
-func Test_ProcMgr_uname(t *testing.T) {
+func Test_ProcMgr_run_uname(t *testing.T) {
     var mgr = ProcMgr("uname")
     var res = mgr.run()
 
     assertStrEq(t, "", res.stderr, "stderr does not match")
-    assertStrEq(t, "Linux", strings.TrimSpace(res.stdout), "stdout does not match")
+    assertStrEq(t, "Linux\n", res.stdout, "stdout does not match")
+
+    assertPtrIsNull(t, res.err, "err does not match")
+}
+
+
+func Test_ProcMgr_runWithTimeout_uname(t *testing.T) {
+    var mgr = ProcMgr("uname")
+    var res = mgr.runWithTimeout()
+
+    assertStrEq(t, "", res.stderr, "stderr does not match")
+    assertStrEq(t, "Linux\n", res.stdout, "stdout does not match")
+
+    assertPtrIsNull(t, res.err, "err does not match")
+}
+
+func Test_ProcMgr_runWithTimeout_sleep(t *testing.T) {
+    var mgr = ProcMgr("sleep", "2")
+    mgr.timeoutMs = 100  // only allow it to run for 100ms
+    var res = mgr.runWithTimeout()
+
+    assertStrEq(t, "", res.stderr, "stderr does not match")
+    assertStrEq(t, "", res.stdout, "stdout does not match")
 
     assertPtrIsNull(t, res.err, "err does not match")
 }
