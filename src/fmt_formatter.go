a34aa6698fb74d76cefd5f5d1f2767eb79a219c5
Martin Matusiak <numerodix@gmail.com>
2016-07-25 19:45:30 +0200

    catch ping time below zero

--- src/fmt_formatter.go
+++ src/fmt_formatter.go
@@ -11,7 +11,7 @@ type Formatter struct {
 
 func (ft *Formatter) formatPingTime(pingExec PingExecution) string {
     // XXX: why is Err always nil?
-    if pingExec.Err != nil || pingExec.Time == 0.0 {
+    if pingExec.Err != nil || pingExec.Time <= 0.0 {
         return ft.colorBrush.red("failed")
     }
 
