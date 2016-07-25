f39b959053797c2665d2f4145e0e9d411bace983
Martin Matusiak <numerodix@gmail.com>
2016-07-25 18:15:49 +0200

    disambiguate name

--- src/net4_linux_ping_test.go
+++ src/net4_linux_ping_test.go
@@ -6,7 +6,7 @@ import "strconv"
 import "testing"
 
 
-const ping4Output = `
+const linuxPing4Output = `
 PING yahoo.com (98.138.253.109) 56(84) bytes of data.
 64 bytes from ir1.fp.vip.ne1.yahoo.com (98.138.253.109): icmp_seq=1 ttl=48 time=154 ms
 
@@ -19,7 +19,7 @@ rtt min/avg/max/mdev = 154.327/154.327/154.327/0.000 ms
 func Test_linuxParsePing4(t *testing.T) {
     var ctx = TestAppContext()
     var pinger = NewLinuxPinger4(ctx)
-    var pingExec = pinger.parsePing4(ping4Output)
+    var pingExec = pinger.parsePing4(linuxPing4Output)
 
     assertStrEq(t, "yahoo.com", pingExec.Host, "wrong host")
     assertStrEq(t, "154.327", strconv.FormatFloat(pingExec.Time, 'f', 3, 64), "wrong time")
