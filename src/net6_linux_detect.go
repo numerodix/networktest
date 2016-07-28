17c934bce80afd359603a930e81e342c4bc1ef1a
Martin Matusiak <numerodix@gmail.com>
2016-07-28 12:36:58 +0200

    add linux ip6 addr detection

--- src/net6_linux_detect.go
+++ src/net6_linux_detect.go
@@ -1,6 +1,6 @@
 package main
 
-//import "fmt"
+import "fmt"
 import "net"
 import "regexp"
 import "strings"
@@ -31,6 +31,23 @@ func (lnd LinuxNetDetect6) detectNetConn6() IPNetworkInfo {
 }
 
 
+func (lnd LinuxNetDetect6) detectIpAddr6(info *IPNetworkInfo) {
+    var mgr = ProcMgr("ip", "-6", "addr", "show")
+    var res = mgr.run()
+
+    // The command failed :(
+    if res.err != nil {
+        lnd.ctx.ft.printError("Failed to detect ipv6 network", res.err)
+        return
+    }
+
+    // Extract the output
+    lnd.parseIpAddr6(res.stdout, info)
+
+    // Parsing failed :(
+    lnd.ctx.ft.printErrors("Failed to parse ipv6 network info", info.Errs)
+}
+
 func (lnd LinuxNetDetect6) detectIpRoute6(info *IPNetworkInfo) {
     var mgr = ProcMgr("ip", "-6", "route", "show")
     var res = mgr.run()
@@ -46,6 +63,67 @@ func (lnd LinuxNetDetect6) detectIpRoute6(info *IPNetworkInfo) {
 
     // Parsing failed :(
     lnd.ctx.ft.printErrors("Failed to parse ipv6 route info", info.Errs)
+}
+
+
+func (lnd LinuxNetDetect6) parseIpAddr6(stdout string, info *IPNetworkInfo) {
+    /* Output:
+      $ /sbin/ip -6 addr show
+      1: lo: <LOOPBACK,UP,LOWER_UP> mtu 16436 
+          inet6 ::1/128 scope host 
+             valid_lft forever preferred_lft forever
+      2: eth0: <BROADCAST,MULTICAST,UP,LOWER_UP> mtu 1500 qlen 1000
+          inet6 2a00:a21f:41::a13/64 scope global 
+             valid_lft forever preferred_lft forever
+          inet6 fe80::a31:49ef:12ce:f5a1/64 scope link 
+             valid_lft forever preferred_lft forever
+    */
+
+    // We will read line by line
+    var lines = strings.Split(stdout, "\n")
+
+    // Prepare regex objects
+    rxIface := regexp.MustCompile("^[0-9]+: ([^ ]+):")
+    rxInet := regexp.MustCompile("^[ ]{4}inet6 ([A-Fa-f0-9:]+)[/]([0-9]+)")
+
+    // Loop variables
+    var iface = ""
+    var ip = ""
+    var maskBits = ""
+
+    for _, line := range lines {
+        if rxIface.MatchString(line) {
+            iface = rxIface.FindStringSubmatch(line)[1]
+        }
+
+        if rxInet.MatchString(line) {
+            ip = rxInet.FindStringSubmatch(line)[1]
+            maskBits = rxInet.FindStringSubmatch(line)[2]
+
+            var ipNet = fmt.Sprintf("%s/%s", ip, maskBits)
+            var ipobj, ipnet, err = net.ParseCIDR(ipNet)
+
+            // Parse failed
+            if err != nil {
+                info.Errs = append(info.Errs, err)
+                continue
+            }
+
+            var netMasked = applyMask(&ipnet.IP, &ipnet.Mask)
+            var mask = ipnetMaskAsIP(ipnet)
+
+            // Populate info
+            info.Nets = append(info.Nets, Network{
+                Iface: Interface{Name: iface},
+                Ip: netMasked,
+            })
+            info.Ips = append(info.Ips, IpAddr{
+                Iface: Interface{Name: iface},
+                Ip: ipobj,
+                Mask: mask,
+            })
+        }
+    }
 }
 
 
--- src/net6_linux_detect_test.go
+++ src/net6_linux_detect_test.go
@@ -5,11 +5,70 @@ package main
 import "testing"
 
 
+const ip6AddrOutput = `
+1: lo: <LOOPBACK,UP,LOWER_UP> mtu 16436 
+    inet6 ::1/128 scope host 
+       valid_lft forever preferred_lft forever
+2: eth0: <BROADCAST,MULTICAST,UP,LOWER_UP> mtu 1500 qlen 1000
+    inet6 2a00:a21f:41::a13/64 scope global 
+       valid_lft forever preferred_lft forever
+    inet6 fe80::a31:49ef:12ce:f5a1/64 scope link 
+       valid_lft forever preferred_lft forever
+`
+
+
 const ip6RouteOutput = `
 2a00:a21f:41::/64 dev eth0  proto kernel  metric 256 
 fe80::/64 dev eth0  proto kernel  metric 256 
 default via 2a00:a21f:41::1 dev eth0  metric 1024 
 `
+
+
+func Test_linuxParseIpAddr6(t *testing.T) {
+    var info = IPNetworkInfo{}
+
+    var ctx = TestAppContext()
+    var detector = NewLinuxNetDetect6(ctx)
+    detector.parseIpAddr6(ip6AddrOutput, &info)
+    info.normalize()
+
+    // Errors
+    assertIntEq(t, 0, len(info.Errs), "Errs does not match")
+
+    // Networks
+    assertIntEq(t, 3, len(info.Nets), "wrong number of networks")
+
+    assertStrEq(t, "lo", info.Nets[0].Iface.Name, "Iface does not match")
+    assertStrEq(t, "::1", info.Nets[0].Ip.IP.String(), "Ip does not match")
+    assertStrEq(t, "ffffffffffffffffffffffffffffffff",
+                    info.Nets[0].Ip.Mask.String(), "Mask does not match")
+
+    assertStrEq(t, "eth0", info.Nets[1].Iface.Name, "Iface does not match")
+    assertStrEq(t, "2a00:a21f:41::", info.Nets[1].Ip.IP.String(), "Ip does not match")
+    assertStrEq(t, "ffffffffffffffff0000000000000000",
+                    info.Nets[1].Ip.Mask.String(), "Mask does not match")
+
+    assertStrEq(t, "eth0", info.Nets[2].Iface.Name, "Iface does not match")
+    assertStrEq(t, "fe80::", info.Nets[2].Ip.IP.String(), "Ip does not match")
+    assertStrEq(t, "ffffffffffffffff0000000000000000",
+                    info.Nets[2].Ip.Mask.String(), "Mask does not match")
+
+    // Ips
+    assertIntEq(t, 3, len(info.Ips), "wrong number of ips")
+
+    assertStrEq(t, "lo", info.Ips[0].Iface.Name, "Iface does not match")
+    assertStrEq(t, "::1", info.Ips[0].Ip.String(), "Ip does not match")
+    assertStrEq(t, "ffff:ffff:ffff:ffff:ffff:ffff:ffff:ffff",
+                    info.Ips[0].Mask.String(), "Mask does not match")
+
+    assertStrEq(t, "eth0", info.Ips[1].Iface.Name, "Iface does not match")
+    assertStrEq(t, "2a00:a21f:41::a13", info.Ips[1].Ip.String(), "Ip does not match")
+    assertStrEq(t, "ffff:ffff:ffff:ffff::", info.Ips[1].Mask.String(), "Mask does not match")
+
+    assertStrEq(t, "eth0", info.Ips[2].Iface.Name, "Iface does not match")
+    assertStrEq(t, "fe80::a31:49ef:12ce:f5a1", info.Ips[2].Ip.String(), "Ip does not match")
+    assertStrEq(t, "ffff:ffff:ffff:ffff::", info.Ips[2].Mask.String(), "Mask does not match")
+}
 
 
 func Test_linuxParseIpRoute6(t *testing.T) {
